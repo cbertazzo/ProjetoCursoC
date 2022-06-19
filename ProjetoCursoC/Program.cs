89fe17c967562da45344fe5df23beecade9787ee
iHost <iHost@DESKTOP-HSMP7U9>
2022-06-19 20:00:43 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoCursoC/Program.cs
@@ -0,0 +1,30 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
+
+namespace PrimeirProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1000,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+
+    }
+}
+
